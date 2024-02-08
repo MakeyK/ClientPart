dcd535c1c7945e0c3426d5882264ae540147b94a
MakeyK <zevstvsrgk@gmail.com>
2024-02-08 10:29:30 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom"; 
 import AppRouter from "./components/AppRouter";  
 import NavBar from "./components/NavBar"; 
-
+import NavBaar_niz from "./components/NavBar1"
 function App() { 
   return ( 
     <BrowserRouter> 
       <NavBar/>
       <AppRouter/> 
+      <NavBaar_niz/>
+
  
     </BrowserRouter> 
   ); 
